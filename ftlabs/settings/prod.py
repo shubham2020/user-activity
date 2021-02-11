b2211b2064f1ce0c5a5f434850cfe608d3d184f5
Shubham <pshubham.me@gmail.com>
2021-02-11 13:48:51 +0530

    enabled HTTPS and HSTS security features

--- ftlabs/settings/prod.py
+++ ftlabs/settings/prod.py
@@ -14,3 +14,13 @@ db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 # Security settings
+
+# HTTPS settings
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+
+# HSTS settings
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
