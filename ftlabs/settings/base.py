922ef3c43b9d54b138e0516ccc5faf55c56e8d99
Shubham <pshubham.me@gmail.com>
2021-02-11 12:24:58 +0530

    add static collection

--- ftlabs/settings/base.py
+++ ftlabs/settings/base.py
@@ -119,7 +119,7 @@ STATIC_URL = '/static/'
 #location where django collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
