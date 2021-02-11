9f1dca2d13a8448658271ada7dc21b49048d1f26
Shubham <pshubham.me@gmail.com>
2021-02-11 11:51:00 +0530

    db changes and allowed hosts changes in production settings

--- ftlabs/settings/prod.py
+++ ftlabs/settings/prod.py
@@ -3,7 +3,12 @@ import dj_database_url
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['user-activiti.herokuapp.com']
+
+DATABASES = {
+    'default':{}
+}
 
 db_from_env = dj_database_url.config(conn_max_age=600)
+
 DATABASES['default'].update(db_from_env)
